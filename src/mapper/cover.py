6d064627052852b49b4583847f602193193bb160
Luca Simi <lucasimi90@gmail.com>
2022-02-22 23:03:44 +0100

    Improving cover methods

--- src/mapper/cover.py
+++ src/mapper/cover.py
@@ -23,6 +23,21 @@ class SearchCover:
                 groups.append(neighbors_ids)
         return groups
 
+    def cover_points(self, data):
+        c = 0
+        data_ids = list(range(len(data)))
+        metric_ids = lambda i, j: self.__metric(self.__lens(data[i]), self.__lens(data[j]))
+        self.__search_algo.setup(data_ids, metric_ids)
+        cover_arr = [[] for _ in data]
+        for i in data_ids:
+            cover_i = cover_arr[i]
+            if not cover_i:
+                neighs = self.__search_algo.find_neighbors(i)
+                for n in neighs:
+                    cover_arr[n].append(c)
+                c += 1
+        return cover_arr
+
 
 class TrivialCover:
 
@@ -30,4 +45,7 @@ class TrivialCover:
         """Return a trivial grouping of data with a single ball"""
         data_ids = list(range(len(data)))
         return [data_ids]
+
+    def cover_points(self, data):
+        return [[0] for _ in data]
 
