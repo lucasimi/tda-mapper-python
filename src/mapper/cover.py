36badd0cd1a996b6648c4f7eb3a7b189118af576
Luca Simi <lucasimi90@gmail.com>
2022-02-25 19:49:15 +0100

    Improved file structure

--- src/mapper/cover.py
+++ src/mapper/cover.py
@@ -9,20 +9,6 @@ class SearchCover:
         self.__metric = metric
         self.__search_algo = search_algo
 
-    def cover(self, data):
-        data_ids = list(range(len(data)))
-        metric_ids = lambda i, j: self.__metric(self.__lens(data[i]), self.__lens(data[j]))
-        self.__search_algo.setup(data_ids, metric_ids)
-        non_covered_ids = {i for i in data_ids}
-        groups = []
-        while non_covered_ids:
-            point_id = non_covered_ids.pop()
-            neighbors_ids = self.__search_algo.find_neighbors(point_id)
-            non_covered_ids.difference_update(neighbors_ids)
-            if neighbors_ids:
-                groups.append(neighbors_ids)
-        return groups
-
     def cover_points(self, data, clusterer):
         c = 0
         data_ids = list(range(len(data)))
@@ -45,12 +31,19 @@ class SearchCover:
         return cover_arr
 
 
-class TrivialCover:
+class ClusterLabels:
 
-    def cover(self, data):
-        """Return a trivial grouping of data with a single ball"""
-        data_ids = list(range(len(data)))
-        return [data_ids]
+    def __init__(self, labels):
+        self.labels_ = labels
+
+
+class TrivialClustering:
+
+    def fit(self, data):
+        return ClusterLabels([0 for _ in data])
+
+
+class TrivialCover:
 
     def cover_points(self, data, clusterer):
         clusters = clusterer.fit(data)
