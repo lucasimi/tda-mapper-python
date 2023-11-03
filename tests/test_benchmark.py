debe2514901d08e39cd8c36dec5fdda904a1f0b3
Luca Simi <lucasimi90@gmail.com>
2023-11-03 21:29:13 +0100

    Improved benchmark

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -20,14 +20,16 @@ def dataset(dim=10, num=1000):
 
 class TestBenchmark(unittest.TestCase):
 
-    logger = logging.getLogger(__name__)
+    eps = 0.25
 
+    k = 5
+
+    logger = logging.getLogger(__name__)
 
     logging.basicConfig(
         format = '%(asctime)s %(module)s %(levelname)s: %(message)s',
         datefmt = '%m/%d/%Y %I:%M:%S %p', 
         level = logging.INFO)
-
 
     def testBench(self):
         self._testCompare(dataset(), 'random')
@@ -37,21 +39,30 @@ class TestBenchmark(unittest.TestCase):
         self._testCompare(list(breast_cancer.to_numpy()), 'breast_cancer')
     
     def _testCompare(self, data, name):
-        self._testBench(data, f' VPT {name}', VPT)
-        self._testBench(data, f'FVPT {name}', FVPT)
+        vpt = self._testBuild(data, f' VPT {name}', VPT)
+        fvpt = self._testBuild(data, f'FVPT {name}', FVPT)
+        self._testBallSearch(data, f' VPT {name}', vpt)
+        self._testBallSearch(data, f'FVPT {name}', fvpt)
+        self._testKNNSearch(data, f' VPT {name}', vpt)
+        self._testKNNSearch(data, f'FVPT {name}', fvpt)
 
-    def _testBench(self, data, name, builder):
-        eps = 0.25
-        k = 5
+    def _testBuild(self, data, name, builder):
         t0 = time()
-        vpt = builder(dist, data, leaf_radius=eps, leaf_size=k)
+        vpt = builder(dist, data, leaf_radius=self.eps, leaf_size=self.k)
         t1 = time()
-        for val in data:
-            neigh = vpt.ball_search(val, eps)
-        t2 = time()
-        for val in data:
-            neigh = vpt.knn_search(val, k)
-        t3 = time()
         self.logger.info(f'{name} build      : {t1 - t0}')
-        self.logger.info(f'{name} ball search: {t2 - t1}')
-        self.logger.info(f'{name} knn search : {t3 - t2}')
+        return vpt
+
+    def _testBallSearch(self, data, name, vpt):
+        t0 = time()
+        for val in data:
+            neigh = vpt.ball_search(val, self.eps)
+        t1 = time()
+        self.logger.info(f'{name} ball search: {t1 - t0}')
+
+    def _testKNNSearch(self, data, name, vpt):
+        t0 = time()
+        for val in data:
+            neigh = vpt.knn_search(val, self.k)
+        t1 = time()
+        self.logger.info(f'{name} knn search : {t1 - t0}')
