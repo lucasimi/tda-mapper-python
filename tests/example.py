9f0bc8b9e20b3ab09bce326897dfa477777df042
Luca Simi <lucasimi90@gmail.com>
2024-01-31 07:37:01 +0100

    Minor fix

--- tests/example.py
+++ tests/example.py
@@ -10,9 +10,9 @@ from tdamapper.clustering import PermissiveClustering
 from tdamapper.plot import MapperPlot
 
 # We load a labelled dataset
-X, y = load_digits(return_X_y=True)             
+X, y = load_digits(return_X_y=True)
 # We compute the lens values
-lens = PCA(2).fit_transform(X)                  
+lens = PCA(2).fit_transform(X)
 
 mapper_algo = MapperAlgorithm(
     cover=CubicalCover(
@@ -26,22 +26,22 @@ mapper_graph = mapper_algo.fit_transform(X, lens)
 
 mapper_plot = MapperPlot(X, mapper_graph,
     # We color according to digit values
-    colors=y,                                   
+    colors=y,
     # Jet colormap, used for classes
-    cmap='jet',                                 
+    cmap='jet',
     # We aggregate on graph nodes according to mean
-    agg=np.nanmean,                             
+    agg=np.nanmean,
     dim=2,
     iterations=400)
 fig_mean = mapper_plot.plot(title='digit (mean)', width=600, height=600)
-fig_mean.show(config={'scrollZoom': True})     
+fig_mean.show(config={'scrollZoom': True})
 
 # We reuse the graph plot with the same positions
-fig_std = mapper_plot.with_colors(              
+fig_std = mapper_plot.with_colors(
     colors=y,
     # Viridis colormap, used for ranges
-    cmap='viridis',                             
+    cmap='viridis',
     # We aggregate on graph nodes according to std
-    agg=np.nanstd,                              
+    agg=np.nanstd,
 ).plot(title='digit (std)', width=600, height=600)
-fig_std.show(config={'scrollZoom': True})      
+fig_std.show(config={'scrollZoom': True})
