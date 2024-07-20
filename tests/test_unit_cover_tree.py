f614b2a6dcb14146bb99ef67ae51186ac024945f
Luca Simi <lucasimi90@gmail.com>
2024-07-20 18:38:06 +0200

    Improved tests

--- tests/test_unit_cover_tree.py
+++ tests/test_unit_cover_tree.py
@@ -15,10 +15,19 @@ class TestCoverTree(unittest.TestCase):
         X = list(np.array([
             [0.0], [1.0], [2.0], [3.0], [4.0], [5.0], [6.0], [7.0]
         ]))
-        ct = CoverTree(X, euclidean())
+        dist = euclidean()
+        ct = CoverTree(X, dist)
         l_max = ct.get_max_level()
         l_min = ct.get_min_level()
         l_max_points = ct.get_level_points(l_max)
         l_min_points = ct.get_level_points(l_min)
         self.assertEqual(1, len(l_max_points))
         self.assertEqual(len(X), len(l_min_points))
+
+        for i in range(l_min, l_max + 1):
+            i_level_points = ct.get_level_points(i)
+            for p in i_level_points:
+                for q in i_level_points:
+                    if not p == q:
+                        d = dist(p, q)
+                        self.assertGreaterEqual(d, 2**i)
