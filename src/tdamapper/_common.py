1314b7a12591f5ea00ffb0a2dc433bb019702f95
Luca Simi <lucasimi90@gmail.com>
2024-11-08 08:26:00 +0100

    Integrated validation code via EstimatorMixin

--- src/tdamapper/_common.py
+++ src/tdamapper/_common.py
@@ -3,6 +3,8 @@ This module provides common functionalities for internal use.
 """
 import warnings
 
+import numpy as np
+
 
 def warn_deprecated(deprecated, substitute):
     msg = f'{deprecated} is deprecated and will be removed in a future version. Use {substitute} instead.'
@@ -15,6 +17,52 @@ def warn_deprecated(deprecated, substitute):
 
 def warn_user(msg):
     warnings.warn(msg, UserWarning, stacklevel=2)
+
+
+class EstimatorMixin:
+
+    def _is_sparse(self, X):
+        # simple alternative use scipy.sparse.issparse
+        return hasattr(X, 'toarray')
+
+    def _validate_X_y(self, X, y):
+        X = np.asarray(X)
+        y = np.asarray(y)
+
+        if self._is_sparse(X):
+            raise ValueError('Sparse data not supported.')
+
+        if X.size == 0:
+            msg = f'0 feature(s) (shape={X.shape}) while a minimum of 1 is required.'
+            raise ValueError(msg)
+
+        if y.size == 0:
+            msg = f'0 feature(s) (shape={y.shape}) while a minimum of 1 is required.'
+            raise ValueError(msg)
+
+        if X.ndim == 1:
+            raise ValueError('1d-arrays not supported.')
+
+        if np.iscomplexobj(X) or np.iscomplexobj(y):
+            raise ValueError('Complex data not supported.')
+
+        if X.dtype == np.object_:
+            X = np.array(X, dtype=float)
+
+        if y.dtype == np.object_:
+            y = np.array(y, dtype=float)
+
+        if np.isnan(X).any() or np.isinf(X).any() or \
+                np.isnan(y).any() or np.isinf(y).any():
+            raise ValueError('NaNs or infinite values not supported.')
+
+        return X, y
+
+    def fit(self, X, y=None):
+        X, y = self._validate_X_y(X, y)
+        res = super().fit(X, y)
+        self.n_features_in_ = X.shape[1]
+        return res
 
 
 class ParamsMixin:
