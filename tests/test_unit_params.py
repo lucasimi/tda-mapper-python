b2fbacd0ea8b501c21af3029bb2d39c037def4c7
Luca Simi <lucasimi90@gmail.com>
2024-12-02 21:16:27 +0100

    Fixed clone and repr. Added tests.

--- tests/test_unit_params.py
+++ tests/test_unit_params.py
@@ -1,5 +1,8 @@
 import unittest
 
+from sklearn.cluster import DBSCAN
+
+from tdamapper._common import clone
 from tdamapper.core import MapperAlgorithm
 from tdamapper.cover import (
     BallCover,
@@ -11,7 +14,23 @@ from tdamapper.clustering import MapperClustering
 
 class TestParams(unittest.TestCase):
 
-    def test_params_mapper(self):
+    def __test_clone(self, obj):
+        obj_repr = repr(obj)
+        obj_cln = clone(obj)
+        cln_repr = repr(obj_cln)
+        self.assertEquals(obj_repr, cln_repr)
+
+    def __test_repr(self, obj):
+        obj_repr = repr(obj)
+        _obj = eval(obj_repr)
+        _obj_repr = repr(_obj)
+        self.assertEquals(obj_repr, _obj_repr)
+
+    def __test_clone_and_repr(self, obj):
+        self.__test_clone(obj)
+        self.__test_repr(obj)
+
+    def test_params_mapper_algorithm(self):
         est = MapperAlgorithm(
             cover=CubicalCover(
                 n_intervals=3,
@@ -30,7 +49,7 @@ class TestParams(unittest.TestCase):
         self.assertEquals(2, params['cover__n_intervals'])
         self.assertEquals(0.2, params['cover__overlap_frac'])
 
-    def test_params_clust(self):
+    def test_params_mapper_clustering(self):
         est = MapperClustering(
             cover=CubicalCover(
                 n_intervals=3,
@@ -48,3 +67,57 @@ class TestParams(unittest.TestCase):
         self.assertEquals(10, len(params))
         self.assertEquals(2, params['cover__n_intervals'])
         self.assertEquals(0.2, params['cover__overlap_frac'])
+
+    def test_clone_and_repr_ball_cover(self):
+        self.__test_clone_and_repr(BallCover())
+        self.__test_clone_and_repr(BallCover(
+            radius=2.0,
+            metric='test',
+            metric_params={'f': 4},
+            kind='kind_test',
+            leaf_capacity=3.0,
+            leaf_radius=-2.0,
+            pivoting=7,
+        ))
+
+    def test_clone_and_repr_cubical_cover(self):
+        self.__test_clone_and_repr(CubicalCover())
+        self.__test_clone_and_repr(CubicalCover(
+            n_intervals=4,
+            overlap_frac=5,
+            algorithm='algo_test',
+            kind='simple',
+            leaf_radius=5,
+            leaf_capacity=6,
+            pivoting='no'
+        ))
+
+    def test_clone_repr_mapper_algorithm(self):
+        self.__test_clone_and_repr(MapperAlgorithm())
+        self.__test_clone_and_repr(MapperAlgorithm(
+            cover=CubicalCover(
+                n_intervals=3,
+                overlap_frac=0.3,
+            ),
+            clustering=DBSCAN(
+                eps='none',
+                min_samples=5.4,
+            ),
+            failsafe=4,
+            n_jobs='foo',
+            verbose=4,
+        ))
+
+    def test_clone_repr_mapper_clustering(self):
+        self.__test_clone_and_repr(MapperClustering())
+        self.__test_clone_and_repr(MapperClustering(
+            cover=CubicalCover(
+                n_intervals=3,
+                overlap_frac=0.3,
+            ),
+            clustering=DBSCAN(
+                eps='none',
+                min_samples=5.4,
+            ),
+            n_jobs='foo',
+        ))
